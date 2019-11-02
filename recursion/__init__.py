5d6446a97c32e1cc5cc27ae386dabd5d3454de24
sorXCode <sorxcode@gmail.com>
2019-11-02 21:41:48 +0100

    day1

--- recursion/__init__.py
+++ recursion/__init__.py
@@ -6,4 +6,41 @@ def factorial(n: int):
     """
     if n==0:
         return 1
-    return n * (n - 1)
+    return n * factorial(n - 1)
+
+
+def binary_search(data, target: int, low: int, high: int):
+    """
+    Given a sorted data, algorithm finds a target position in O(logn)
+    params:
+        -data (iterable int)
+    """
+    # End search if interval empty; i.e match not found
+    if low > high:
+        return False
+    mid = (low + high)//2
+    mid_value = data[mid]
+    if target == mid_value:
+        return True
+    elif target < mid_value:
+        # recur to the left of the middle
+        return binary_search(data, target, low, mid-1)
+    else:
+        # recur to the right of the middle
+        return binary_search(data, target, mid+1, high)
+
+
+def disk_usage(path):
+    """
+    Returns the total size of file/folder and any subfolder
+    """
+    import os
+    total = os.path.getsize(path)
+    if os.path.isdir(path):
+        # on my pc each folder is approximately 4kb excluding content
+        total += 4096
+        for filename in os.listdir(path):
+            childpath = os.path.join(path, filename)
+            # compute size of childpath/directory via recursion and sum with total
+            total += disk_usage(childpath)
+    return total
--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,34 @@
 import unittest
-from recursion import factorial
+from random import randint
+from recursion import *
+
 
 class TestCases(unittest.TestCase):
     def test_factorial(self):
         self.assertEqual(factorial(5), 120, '5! should be 120')
+
+    def test_binary_search(self):
+        data = sorted(set([randint(1, 100) for x in range(50)]))
+        target = data[randint(0, len(data)-1)]
+        self.assertEqual(binary_search(
+            data, target, 0, len(data)), True)
+
+    def test_disk_usage(self):
+        import os
+        import re
+        import subprocess
+        # get  disk_usage_size using funtion
+        current_path = os.getcwd()
+        disk_usage_size = disk_usage(current_path)/1000
+        # get summary of current path from console/terminal
+        console_report = str(subprocess.run(
+            f"du -sx '{current_path}'", shell=True, capture_output=True))
+        reported_size = re.search(
+            r'stdout=b\'(\d+)', console_report).groups()[0]
+        reported_size = int(reported_size)
+        # compare values
+        self.assertAlmostEqual(disk_usage_size, reported_size, delta=2)
+
 
 if __name__ == "__main__":
     unittest.main()
