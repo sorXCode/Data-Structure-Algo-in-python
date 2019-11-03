bf0e3345f183578b018f3811dcf9430ef5ff9e1b
sorXCode <sorxcode@gmail.com>
2019-11-03 20:59:11 +0100

    day 2

--- recursion/__init__.py
+++ recursion/__init__.py
@@ -3,6 +3,7 @@ def factorial(n: int):
     Returns the factorial of any number given
     params:
         -n (int)
+    time: O(n)
     """
     if n==0:
         return 1
@@ -14,6 +15,7 @@ def binary_search(data, target: int, low: int, high: int):
     Given a sorted data, algorithm finds a target position in O(logn)
     params:
         -data (iterable int)
+    time: O(log n)
     """
     # End search if interval empty; i.e match not found
     if low > high:
@@ -33,7 +35,11 @@ def binary_search(data, target: int, low: int, high: int):
 def disk_usage(path):
     """
     Returns the total size of file/folder and any subfolder
+    params:
+        -path
+    time: O(n)(amortization/tree traversal)
     """
+
     import os
     total = os.path.getsize(path)
     if os.path.isdir(path):
@@ -44,3 +50,110 @@ def disk_usage(path):
             # compute size of childpath/directory via recursion and sum with total
             total += disk_usage(childpath)
     return total
+
+def fibonacci(n):
+    """
+    Returns pair of fibonacci number F(n), F(n-1)
+    params:
+        -n (int)
+    time: O(n)
+    """
+    if n <= 1:
+        return (0, n)
+    (b, a) = fibonacci(n-1)
+    return (a, a+b)
+
+def change_max_recursion_depth(n=1000):
+    """
+    Changes the maximum recursion depth in python interpreter
+    params:
+        -n: (int) default is 1000
+    returns:
+        1: success
+        -1: failed
+    time: O(1)
+    """
+    import sys
+    try:
+        sys.setrecursionlimit(n)
+        return 1
+    except Exception:
+        return -1
+
+def linear_sum(S, n):
+    """
+    Returns the sum of the first n terms of sequence S
+    params:
+        - S (iterable)
+        - n (int) stop position
+    time: O(n)
+    space: O(n)
+    """
+    if n<=0:
+        return 0
+    return linear_sum(S, n-1) + S[n-1]
+
+def reverse(S, start, stop):
+    """
+    Reverse elements in a sequence slice
+    params:
+        - S (iterable)
+        - start (int)
+        - stop (int)
+    time: O(n)
+    """
+    if start < stop - 1:
+        S[start], S[stop-1] = S[stop-1], S[start]
+        reverse(S, start+1, stop-1)
+    return S
+
+def power(x, n):
+    """
+    Returns the x**n
+    params:
+        - x (int)
+        - n (int)
+    time: O(n)
+    """
+    if n == 0:
+        return 1
+    return x * power(x, n-1)
+
+def power_(x, n):
+    """
+    Returns the x**n
+    k = n//2; when n is even: x^k**2 == x^n
+              when n is odd: x^k**2 == x^n-1 == x.x^n
+    params:
+        - x (int)
+        - n (int)
+    time: O(log n)
+    """
+    if n == 0:
+        return 1
+    else:
+        partial = power_(x, n//2)
+        result = partial * partial
+        if n%2 == 1:
+            result *= x
+        return result
+
+def binary_sum(S, start, stop):
+    """
+    Returns sum of elements in a slice
+    params:
+        - S (iterable)
+        - start (int)
+        - stop (int)
+    space: O(log n)
+    time: O(n)
+    """
+    # no element in slice
+    if start >= stop:
+        return 0
+    # one element in slice
+    elif start == stop - 1:
+        return S[start]
+    else:
+        mid = (start+stop)//2
+        return binary_sum(S, start, mid) + binary_sum(S, mid, stop)
--- tests/tests.py
+++ tests/tests.py
@@ -29,6 +29,33 @@ class TestCases(unittest.TestCase):
         # compare values
         self.assertAlmostEqual(disk_usage_size, reported_size, delta=2)
 
+    def test_fibonacci(self):
+        fibonacci_ = fibonacci(5)
+        a, b = 0, 1
+        for _ in range(1, 5):
+            a, b = b, b + a
+        self.assertEqual(fibonacci_, (a,b))
+
+    def test_linear_sum(self):
+        sequence = [_ for _ in range(100)]
+        self.assertEqual(linear_sum(sequence, 10), sum(sequence[:10]))
+
+    def test_reverse(self):
+        n = 20
+        sequence = [_ for _ in range(100)]
+        reversed_nth = sequence[n-1:0:-1] #reversing using slicing
+        function_reversed_nth = reverse(sequence, 1, n)[1:n] #slicing only reversed part
+        self.assertEqual(function_reversed_nth, reversed_nth)
+
+    def test_power(self):
+        self.assertEqual(power(3,5), pow(3,5))
+
+    def test_power_(self):
+        self.assertEqual(power_(3, 5), pow(3,5))
+
+    def test_binary_sum(self):
+        sequence = [_ for _ in range(100)]
+        self.assertEqual(binary_sum(sequence, 1, 20), sum(sequence[1:20]))
 
 if __name__ == "__main__":
     unittest.main()
